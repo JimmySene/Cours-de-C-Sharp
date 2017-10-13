7a5528ac60450e26194f936b01028548f69b10d0
Jimmy Sené <jimmy.sene@laposte.net>
2017-10-13 11:07:37 +0200

    Tableau multi dimensions.

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { // TABLEAU MULTI DIMENSIONS 
+
+            int nombreLignes, nombreColones;
+
+            Console.WriteLine("Entrez le nombre de lignes : ");
+            nombreLignes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez le nombre de colones : ");
+            nombreColones = int.Parse(Console.ReadLine());
+
+            Random aleatoire = new Random();
+
+            int[,] tableau = new int[nombreLignes, nombreColones];
+
+            for(int i = 0; i < nombreLignes; i++)
+            {
+                for(int j = 0; j < nombreColones; j++)
+                {
+                    tableau[i, j] = aleatoire.Next(0,100);
+                    Console.Write(tableau[i, j]+ " ");
+                }
+                Console.WriteLine("");
+            }
+
+            
+            Console.ReadKey(); 
+
+        
+
+        } 
+    }
+}
