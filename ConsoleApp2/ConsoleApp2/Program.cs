3b75ad356f4af46ec54ad44facc0949463fe4d97
Jimmy Sené <jimmy.sene@laposte.net>
2017-10-20 12:01:49 +0200

    Application tableau - découverte des procédures et fonctions.

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -5,34 +5,85 @@ namespace ConsoleApp2
     class Program
     {
         static void Main(string[] args)
-        { // TABLEAU MULTI DIMENSIONS 
+        { 
 
-            int nombreLignes, nombreColones;
+            int[] monTab  = new int[10];
 
-            Console.WriteLine("Entrez le nombre de lignes : ");
-            nombreLignes = int.Parse(Console.ReadLine());
-            Console.WriteLine("Entrez le nombre de colones : ");
-            nombreColones = int.Parse(Console.ReadLine());
+            remplirTab(ref monTab);
+            triTab(ref monTab);
+            afficherTab(ref monTab);
+          
+
+            Console.WriteLine("La valeur min vaut " + donnerMin(monTab));
+            Console.WriteLine("La valeur max vaut " + donnerMax(monTab));
+
+            Console.ReadKey(); 
+
+        }
+
+        static void afficherTab(ref int[] tab)
+        {
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.WriteLine(tab[i]);
+            }
+        }
+        static int donnerMin(int[] tab)
+        {
+            int min = tab[0];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i] < min)
+                {
+                    min = tab[i];
+                }
+            }
+            return min;
+        }
+       static int donnerMax(int[] tab)
+        {
+            int max = tab[0];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i] > max)
+                {
+                    max = tab[i];
+                }
+            }
+            return max;
+        }
+
+        static void remplirTab(ref int[] tab)
+        {
 
             Random aleatoire = new Random();
 
-            int[,] tableau = new int[nombreLignes, nombreColones];
-
-            for(int i = 0; i < nombreLignes; i++)
+            for (int i = 0; i < 10; i++)
             {
-                for(int j = 0; j < nombreColones; j++)
-                {
-                    tableau[i, j] = aleatoire.Next(0,100);
-                    Console.Write(tableau[i, j]+ " ");
-                }
-                Console.WriteLine("");
+
+                tab[i] = aleatoire.Next(0, 100);
+
             }
+        }
 
-            
-            Console.ReadKey(); 
+        static void triTab(ref int[] tab)
+        {
 
-        
+            int temp = 0;
 
-        } 
+            for(int i = 0; i < tab.Length; i++)
+            {
+                for(int j = i + 1; j < tab.Length; j++)
+                {
+                    if (tab[j] < tab[i])
+                    {
+                        temp = tab[i];
+                        tab[i] = tab[j];
+                        tab[j] = temp;
+                    }
+                }  
+            }
+        }
+
     }
 }
