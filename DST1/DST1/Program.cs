52dd46be759d9e812a21a838b29e014b52fb9d90
Jimmy Sené <jimmy.sene@laposte.net>
2017-11-24 09:37:02 +0100

    TP 3 - Trie tab à 2 dimensions.

---
+++ DST1/DST1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DST1_SENE_Jimmy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbModif = 0;
+            string machaine, newChaine = "";
+            Console.WriteLine("Quelle est votre saisie ?");
+            machaine = Console.ReadLine();
+            
+            for(int i = 0; i < machaine.Length; i++)
+            {
+                if(machaine[i].ToString() == "e")
+                {
+                    nbModif++;
+                    newChaine += "o";
+
+                } else if(machaine[i].ToString() == "o")
+                {
+                    nbModif++;
+                    newChaine += "e";
+                } else
+                {
+                    newChaine += machaine[i];
+                }
+            }
+            Console.WriteLine(machaine);
+            Console.WriteLine("Saisie apres " + nbModif + " modifications : " + newChaine);
+            Console.ReadKey();
+        }
+    }
+}
