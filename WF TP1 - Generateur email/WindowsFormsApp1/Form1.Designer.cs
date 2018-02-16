6d552b9e65be0085b8a5b5ba4b772b4264b32292
Jimmy Sené <jimmy.sene@laposte.net>
2018-02-16 11:55:31 +0100

    Generateur email avec Windows Forms

---
+++ WF TP1 - Generateur email/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_nom = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_prenom = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_societe = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_email = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom : ";
+            // 
+            // tb_nom
+            // 
+            this.tb_nom.Location = new System.Drawing.Point(97, 25);
+            this.tb_nom.Name = "tb_nom";
+            this.tb_nom.Size = new System.Drawing.Size(155, 20);
+            this.tb_nom.TabIndex = 1;
+            this.tb_nom.Leave += new System.EventHandler(this.tb_nom_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Prénom : ";
+            // 
+            // tb_prenom
+            // 
+            this.tb_prenom.Location = new System.Drawing.Point(97, 56);
+            this.tb_prenom.Name = "tb_prenom";
+            this.tb_prenom.Size = new System.Drawing.Size(155, 20);
+            this.tb_prenom.TabIndex = 3;
+            this.tb_prenom.Leave += new System.EventHandler(this.tb_prenom_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Créer une adresse email !";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Societe :";
+            // 
+            // tb_societe
+            // 
+            this.tb_societe.Location = new System.Drawing.Point(97, 93);
+            this.tb_societe.Name = "tb_societe";
+            this.tb_societe.Size = new System.Drawing.Size(155, 20);
+            this.tb_societe.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Email : ";
+            // 
+            // tb_email
+            // 
+            this.tb_email.Location = new System.Drawing.Point(97, 197);
+            this.tb_email.Name = "tb_email";
+            this.tb_email.Size = new System.Drawing.Size(155, 20);
+            this.tb_email.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tb_email);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_societe);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_prenom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_nom);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_nom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_prenom;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_societe;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_email;
+    }
+}
+
---
+++ WF TP1 - Generateur email/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String nom = tb_nom.Text;
+            String prenom = tb_prenom.Text;
+            String societe = tb_societe.Text;
+
+            String email = prenom.Substring(0, 3) + "_" + nom + "@" + societe + ".com";
+            tb_email.Text = email.ToLower();
+        }
+
+        private void tb_nom_Leave(object sender, EventArgs e)
+        {
+            string nom = tb_nom.Text.ToUpper();
+            tb_nom.Text = nom;
+        }
+
+        private void tb_prenom_Leave(object sender, EventArgs e)
+        {
+            string prenom = tb_prenom.Text.ToLower();
+            tb_prenom.Text = prenom;
+        }
+    }
+}
